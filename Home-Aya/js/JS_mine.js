556199bf4ca87e9a99daad2bd03b09db6ef7af54
Marawan Nour <marmoor_55@hotmail.com>
2019-10-19 20:49:06 +0200

    Final Commit

---
+++ Home-Aya/js/JS_mine.js
@@ -0,0 +1,6 @@
+(function showUp(){
+    if(window.innerWidth <= 767){
+    document.getElementById("nav-button-div").classList.add('justify-content-center');
+    document.getElementById("main-icon-div").classList.add('text-center');
+    document.getElementById("footer-ul").classList.remove('d-flex', 'justify-content-around')}
+    })()
